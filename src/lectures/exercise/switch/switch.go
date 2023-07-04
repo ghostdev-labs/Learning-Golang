6c9876eabc805e38c525d3ad2c011a0f5790475e
Allen Enyi <ekeneenyi@gmail.com>
2023-07-04 20:11:13 +0100

    worked on switch statements

--- src/lectures/exercise/switch/switch.go
+++ src/lectures/exercise/switch/switch.go
@@ -15,14 +15,19 @@ import "fmt"
 
 func main() {
 	switch age := 0; {
+// - "newborn" when age is 0
 	case age == 0:
 		fmt.Println("newborn")
+//  - "toddler" when age is 1, 2, or 3
 	case age >= 1 && age <= 3:
 		fmt.Println("toddler")
+//  - "child" when age is 4 through 12
 	case age < 13:
 		fmt.Println("child")
+//  - "teenager" when age is 13 through 17
 	case age < 18:
 		fmt.Println("teenager")
+//  - "adult" when age is 18+
 	default:
 		fmt.Println("adult")
 	}
